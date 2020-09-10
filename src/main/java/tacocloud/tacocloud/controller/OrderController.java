ee6c6b17f6b19797ca08320ac66475d5627490bf
makovey <twomvlad@mail.ru>
2020-09-10 18:55:47 +0300

    fixed bug wih 403 after choose ingredient order knows profile data

--- src/main/java/tacocloud/tacocloud/controller/OrderController.java
+++ src/main/java/tacocloud/tacocloud/controller/OrderController.java
@@ -2,15 +2,14 @@ package tacocloud.tacocloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import tacocloud.tacocloud.entity.Order;
+import tacocloud.tacocloud.entity.User;
 import tacocloud.tacocloud.repositories.OrderRepo;
 
 import javax.validation.Valid;
@@ -29,16 +28,37 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    public String orderForm(Model model,
+                            @AuthenticationPrincipal User user,
+                            @ModelAttribute Order order) {
+
+        if (order.getName() == null) {
+            order.setName(user.getFullName());
+        }
+        if (order.getStreet() == null) {
+            order.setStreet(user.getStreet());
+        }
+        if (order.getCity() == null) {
+            order.setCity(user.getCity());
+        }
+        if (order.getState() == null) {
+            order.setState(user.getState());
+        }
+        if (order.getZip() == null) {
+            order.setZip(user.getZip());
+        }
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+
+        order.setUser(user);
 
         orderRepo.save(order);
         sessionStatus.setComplete();

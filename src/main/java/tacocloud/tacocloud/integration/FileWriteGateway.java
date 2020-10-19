3dcc2a03e14d0d208c834683359f0ff53040c7b8
makovey <twomvlad@mail.ru>
2020-10-19 19:55:47 +0300

    [+] Added FileWriter Integration

---
+++ src/main/java/tacocloud/tacocloud/integration/FileWriteGateway.java
@@ -0,0 +1,11 @@
+package tacocloud.tacocloud.integration;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "textInChannel")
+public interface FileWriteGateway {
+
+    void writeToFile(@Header(FileHeaders.FILENAME) String filename, String data);
+}
